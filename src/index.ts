8b0efcfdde77edb8164432a7880c33177bd3a67d
Zach Becker <zbecker10@gmail.com>
2021-07-15 06:48:31 +0000

    Proper typings and Plugin implementation.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
-import {CookieOptions} from 'express';
+import {ApolloServerPlugin,
+	BaseContext,
+	GraphQLRequestContext,
+	GraphQLRequestContextWillSendResponse,
+	GraphQLRequestListener} from 'apollo-server-plugin-base';
+import {CookieOptions, Response} from 'express';
 
 export interface Cookie {
 	name: string;
 	value: string;
 	options: CookieOptions;
+};
+
+export interface CustomRequestContext extends BaseContext {
+	cookies: Cookie[];
+	res: Response;
 }
 
-export interface CustomRequestContext {
-	context: {
-		cookies: Cookie[];
-		res: any;
-	};
-}
-
-export const CookiePlugin = {
-	requestDidStart() {
+export const CookiePlugin: ApolloServerPlugin = {
+	async requestDidStart(context: GraphQLRequestContext): Promise<GraphQLRequestListener<CustomRequestContext>> {
 		return {
-			willSendResponse(requestContext: CustomRequestContext) {
-				const {cookies = [], res} = requestContext.context;
+			async willSendResponse(
+				context: GraphQLRequestContextWillSendResponse<CustomRequestContext>
+			) {
+				const {cookies = [], res} = context.context;
 				cookies.forEach((cookie) => {
 					res.cookie(cookie.name, cookie.value, cookie.options);
 				});
-				return requestContext;
 			}
 		};
 	}
